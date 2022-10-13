142eba932c9d2f51c91d184bb2a518a35a88a7e1
AP Java Period 6 <apjavaperiod6@ac1mwk1agjc9h.78b505.nycboe.org>
2022-10-13 12:59:06 -0400

    mofified teacher and student code

--- o6-username/Main.java
+++ o6-username/Main.java
@@ -7,41 +7,27 @@ public class Main {
     Scanner s = new Scanner(System.in);
     System.out.println("Hello user, are you a Teacher or a Student? ");
     String answer = s.nextLine();
+    System.out.print("Enter your first name: ");
+    String firstName = s.nextLine();
+    System.out.print("Enter your last name: ");
+    String lastName = s.nextLine();
+    System.out.println("Enter you favorite number: ");
+    int favNumber = s.nextInt();
     if (answer.equalsIgnoreCase("student")) {
-      System.out.print("Enter your first name: ");
-      String firstName = s.nextLine();
-      System.out.print("Enter your last name: ");
-      String lastName = s.nextLine();
-      System.out.println("Enter you favorite number: ");
-      int favNumber = s.nextInt();
-      System.out.println("Your username is:  " + sInitial1(firstName.toLowerCase()) + sInitial2(lastName.toLowerCase()) + favNumber + "@nycstudents.net");
-
+      System.out.println("Your username is:  " + firstName.toLowerCase() + sInitial(lastName.toLowerCase()) + favNumber + "@nycstudents.net");
       s.close();
-
     }
     else {
-      System.out.print("Enter your first name: ");
-      String firstName = s.nextLine();
-      System.out.print("Enter your last name: ");
-      String lastName = s.nextLine();
-      System.out.println("Enter you favorite number: ");
-      int favNumber = s.nextInt();
-      System.out.println("Your username is:  " + tInitial1(firstName.toLowerCase()) + tInitial2(lastName.toLowerCase()) + favNumber + "@schools.nyc.gov");
-
+      System.out.println("Your username is:  " + tInitial(firstName.toLowerCase()) + lastName.toLowerCase() + favNumber + "@schools.nyc.gov");
       s.close();
 
     }
   }
-  public static String sInitial1 (String n){
-    return n;
-  }
-  public static String sInitial2 (String p){
+
+  public static String sInitial (String p){
     return p.substring(0, 1);
   }
-  public static String tInitial1 (String q){
+  public static String tInitial (String q){
     return q.substring(0,1);
-  }
-  public static String tInitial2 (String w){
-    return w;
   }
 }
